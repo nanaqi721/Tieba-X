54fe3e0a481eac866f6478cffbf25cbe5a4c3874
nanaqi <lixiaya721@itcast.cn>
2026-08-04 15:16:42 +0800

    feat：引入mp的属性自动填充

--- post-server/src/main/java/mapper/entiy/Post.java
+++ post-server/src/main/java/mapper/entiy/Post.java
@@ -1,9 +1,6 @@
 package mapper.entiy;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -73,11 +70,13 @@ public class Post {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     /**
      * 修改时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     /**
