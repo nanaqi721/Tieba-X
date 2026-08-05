e20916737a849f186fa353f0125fd10314268102
nanqi721 <lixiaya721@gmail.com>
2026-08-05 16:27:59 +0800

    feat：删除mp中自动填充时打印日志

--- post-server/src/main/java/com/mint/ai/config/MyMetaObjectHandler.java
+++ post-server/src/main/java/com/mint/ai/config/MyMetaObjectHandler.java
@@ -16,14 +16,12 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        log.info("开始插入填充...");
         this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
         this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        log.info("开始更新填充...");
         this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
     }
 }
