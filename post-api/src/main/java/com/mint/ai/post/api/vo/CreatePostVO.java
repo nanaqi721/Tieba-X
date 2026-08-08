9197ef8830d703b16e9226158125c2c95eff8eac
nanqi721 <lixiaya721@gmail.com>
2026-08-08 20:03:07 +0800

    fixed：修复创建评论时新增图片表的url传入错误

---
+++ post-api/src/main/java/com/mint/ai/post/api/vo/CreatePostVO.java
@@ -0,0 +1,22 @@
+package com.mint.ai.post.api.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 帖子响应实体
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class CreatePostVO {
+
+    /**
+     * 帖子id
+     */
+    private String id;
+    // 测试贡献
+}
